c5c8607f6effbdebeded33ef4d3c9cb1cdde29bf
Patrick O'Shea <patso.oshea@gmail.com>
2014-03-05 14:04:37 -0600

    Created records app

--- vinyl/settings.py
+++ vinyl/settings.py
@@ -57,8 +57,10 @@ WSGI_APPLICATION = 'vinyl.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'vinyl',
+        'PASSWORD': 'zero11235',
+        
     }
 }
 
@@ -67,7 +69,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Chicago'
 
 USE_I18N = True
 
